3a355eed6f7873d0adee8230b510512e6cb1544f
San Lee <ls141588@outlook.com>
2025-11-28 11:51:50 +0800

    欢迎界面UI设计开发
    1. 完成欢迎页面UI设计，GreetingActivity 替换原来的MainActivity。
    2. 开启viewBinding功能。
    3. 欢迎页面UI已在设备上测试。

--- app/src/main/java/com/example/tticediting/GreetingActivity.kt
+++ app/src/main/java/com/example/tticediting/GreetingActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tticediting.databinding.GreetingActivityBinding
 
-class MainActivity : AppCompatActivity() {
+class GreetingActivity : AppCompatActivity() {
+    private lateinit var binding: GreetingActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = GreetingActivityBinding.inflate(layoutInflater)
+        initUi()
+    }
+
+    private fun initUi() {
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
